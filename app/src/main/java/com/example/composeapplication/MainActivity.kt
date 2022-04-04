9796a95e0df2ea3af0193126618ce63d3f90b88f
saraebadi <s.ebadi@roundtableapps.com>
2022-04-04 16:10:23 +0430

    Simple Component and Modifier.

--- app/src/main/java/com/example/composeapplication/MainActivity.kt
+++ app/src/main/java/com/example/composeapplication/MainActivity.kt
@@ -1,11 +1,43 @@
 package com.example.composeapplication
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.composeapplication.ui.theme.ComposeApplicationTheme
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+            ComposeApplicationTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Surface(color = MaterialTheme.colors.primary) {
+        Text(text = "Hello $name!", modifier = Modifier.padding(24.dp))
+    }
+}
+
+@Preview(showBackground = true, name = "Text Preview")
+@Composable
+fun DefaultPreview() {
+    ComposeApplicationTheme {
+        Greeting("Android")
     }
 }
